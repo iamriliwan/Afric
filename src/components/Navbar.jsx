4f09e8f8d34d66c0d14a0423b7e269aa2d546606
Riliwan Balogun <riliwan.balogun99@gmail.com>
2021-04-19 11:38:00 +0100

    Refactored code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,39 @@
 import React from 'react'
-import home from '../img/Navbar/home.svg'
+import homeIcon from '../img/Navbar/home.svg'
+import activityIcon from '../img/Navbar/sketchbook.svg'
+import walletIcon from '../img/Navbar/wallet.svg'
+import marketIcon from '../img/Navbar/stock-market.svg';
+import earnIcon from '../img/Navbar/profit.svg';
+
 import { Box, Grid, Text } from '@chakra-ui/layout';
 
 function Navbar(){
     return(
-        <Box p={6}>
-            <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+        <Box p={6} centerContent>
+            <Grid templateColumns="repeat(5, 1fr)" gap={6} >
                 <Box>
-                    <img src={home} alt="home" />
-                    <Text>Home</Text>
+                    <img src={homeIcon} alt="home" />
+                    <Text color="#FB5267" fontWeight="700">Home</Text>
                 </Box>
 
                 <Box>
-                    <img src={home} alt="home" />
-                    <Text>Home</Text>
+                    <img src={activityIcon} alt="activity" />
+                    <Text color="#FB5267" fontWeight="700">Activity</Text>
                 </Box>
 
                 <Box>
-                    <img src={home} alt="home" />
-                    <Text>Home</Text>
+                    <img src={walletIcon} alt="wallet" />
+                    <Text color="#FB5267" fontWeight="700">Wallet</Text>
                 </Box>
 
                 <Box>
-                    <img src={home} alt="home" />
-                    <Text>Home</Text>
+                    <img src={marketIcon} alt="marketIcon" />
+                    <Text color="#FB5267" fontWeight="700">Market</Text>
                 </Box>
 
                 <Box>
-                    <img src={home} alt="home" />
-                    <Text>Home</Text>
+                    <img src={earnIcon} alt="earnIcon" />
+                    <Text color="#FB5267" fontWeight="700">Earn</Text>
                 </Box>
                 
             </Grid>
